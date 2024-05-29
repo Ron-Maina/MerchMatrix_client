cd3a960f19e22c7b02ec0d6d5fb6eec759db8fcf
Ron Maina <ronmwangi00@gmail.com>
2024-05-29 21:22:58 +0300

    Our values to core values

--- src/features/HomePage/Values.js
+++ src/features/HomePage/Values.js
@@ -33,7 +33,7 @@ export default function Values() {
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
           <h2 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Our Values
+            Core Values
           </h2>
           <p className="mt-6 text-lg leading-8 text-gray-600">
             Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum
