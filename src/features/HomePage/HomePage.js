1e7f4e81a1c9850fb2e8154ed920291e2196d9b6
Ron Maina <ronmwangi00@gmail.com>
2024-05-29 18:35:02 +0300

    Render mission and values to homepage

--- src/features/HomePage/HomePage.js
+++ src/features/HomePage/HomePage.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import HeroSection from './HeroSection'
+import Values from './Values'
+import Mission from './Mission'
 
 function HomePage() {
   return (
     <div>
       <HeroSection/>
+      <Mission/>
+      <Values/>
     </div>
   )
 }
