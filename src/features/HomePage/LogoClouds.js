c80b2e05fda888480ef2b295abfd941a4729f392
Ron Maina <ronmwangi00@gmail.com>
2024-05-29 21:20:11 +0300

    Company logos

---
+++ src/features/HomePage/LogoClouds.js
@@ -0,0 +1,57 @@
+import React from 'react'
+
+function LogoClouds() {
+    return (
+        <div className="bg-white py-24 sm:py-32">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <h2 className="text-center text-2xl font-semibold leading-8 text-gray-900">
+              Trusted by the world’s most innovative teams
+            </h2>
+            <br/>
+            <p className="text-center text-lg leading-8 text-gray-900">
+                Arcu cursus vitae congue mauris rhoncus viverra nibh cras pulvinar mattis blandit libero cursus mattis.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan ut quam in congue. Praesent non ornare nibh. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer rhoncus gravida turpis at interdum. Mauris et finibus mauris. Fusce sodales ornare dui, non gravida dui fringilla vel. Duis interdum lectus eu nisi tincidunt, ac laoreet quam sagittis.
+            </p>
+            <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+              <img
+                className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
+                alt="Transistor"
+                width={158}
+                height={48}
+              />
+              <img
+                className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
+                alt="Reform"
+                width={158}
+                height={48}
+              />
+              <img
+                className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
+                alt="Tuple"
+                width={158}
+                height={48}
+              />
+              <img
+                className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+                src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"
+                alt="SavvyCal"
+                width={158}
+                height={48}
+              />
+              <img
+                className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+                src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"
+                alt="Statamic"
+                width={158}
+                height={48}
+              />
+            </div>
+          </div>
+        </div>
+      )
+}
+
+export default LogoClouds
