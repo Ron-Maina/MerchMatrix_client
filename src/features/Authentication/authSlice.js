526c835688837c2d0714be65e88dedddb44e6212
Ron Maina <ronmwangi00@gmail.com>
2024-05-30 16:36:25 +0300

    Created an autentication slice

---
+++ src/features/Authentication/authSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState: {user: null, token: null},
+    reducers: {
+        logIn: (state, action) => {
+            const {user, accessToken} = action.payload
+            state.user = user
+            state.token = accessToken
+        },
+        logOut: (state) => {
+            state.user = null
+            state.token = null
+        }
+    }
+})
+
+export const {logIn, logOut} = authSlice.actions
+
+export default authSlice.reducer
+
+// selector function to get current user and token
+export const selectCurrentUser = (state) => state.auth.user
+export const selectCurrentToken = (state) => state.auth.token
