ddf931a15dee5ae1c24978e04f214c1e3d2abb9b
Ron Maina <ronmwangi00@gmail.com>
2024-05-30 20:13:17 +0300

    created an api slice for rtk query

---
+++ src/app/api/apiSlice.js
@@ -0,0 +1,41 @@
+import { CreateApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { logIn, logOut } from "../../features/Authentication/authSlice";
+import { createApi } from "@reduxjs/toolkit/query";
+
+
+const baseQuery = fetchBaseQuery({
+    baseUrl: 'http://127.0.0.1:5000',
+    credentials: 'include',
+    prepareHeaders: (headers, {getState}) => {
+        const token = getState().auth.token
+        if (token) {
+            headers.set("authorization", `Bearer ${token}`)
+        }
+        return headers
+    }
+})
+
+const baseQueryWithReAuth = async(args, api, extraOptions) => {
+    let result = await baseQuery(args, api, extraOptions)
+
+    if (result?.error?.originalStatus === 403){
+        console.log('sending refresh token')
+
+        const refreshResult = await baseQuery('/refresh', api, extraOptions)
+        console.log(refreshResult)
+        if (refreshResult?.data){
+            const user = api.getState().auth.user
+            api.dispatch(logIn({...refreshResult.data, user}))
+            // retry original query with new access token
+            result = await baseQuery(args, api, extraOptions)
+        } else {
+            api.dispatch(logOut())
+        }
+    }
+    return result
+}
+
+export const apiSlice = createApi({
+    baseQuery: baseQueryWithReAuth,
+    endpoints: builder => ({})
+})
