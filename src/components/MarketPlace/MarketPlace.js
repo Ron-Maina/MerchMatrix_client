f1f31e442d80fe217856de8dbcd2dcf1e7f02cd9
Ron Maina <ronmwangi00@gmail.com>
2024-06-11 13:51:37 +0300

    render products

---
+++ src/components/MarketPlace/MarketPlace.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Products from './Products'
+
+function MarketPlace() {
+    return (
+        <div>
+            <Products />   
+        </div>
+    )
+}
+
+export default MarketPlace
