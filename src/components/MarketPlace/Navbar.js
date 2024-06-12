efbf0677f72c5ab366ed39f855e5a2375c987fd8
Ron Maina <ronmwangi00@gmail.com>
2024-06-12 14:57:09 +0300

    Render navbar in products page and description page

---
+++ src/components/MarketPlace/Navbar.js
@@ -0,0 +1,102 @@
+import React from 'react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+
+    const [openProfile, setOpenProfile] = useState(false)
+
+    return (
+        <nav class="bg-white">
+            <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                <div class="relative flex h-16 items-center justify-between">
+                    <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                        <button type="button" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                        <span class="absolute -inset-0.5"></span>
+                        <span class="sr-only">Open main menu</span>
+                        
+                        <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                        </svg>
+                        
+                        <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                        </button>
+                    </div>
+                    <div className="flex flex-1 items-center justify-between sm:items-stretch">
+                        <div className="flex flex-shrink-0 items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                                <path d="M5.223 2.25c-.497 0-.974.198-1.325.55l-1.3 1.298A3.75 3.75 0 0 0 7.5 9.75c.627.47 1.406.75 2.25.75.844 0 1.624-.28 2.25-.75.626.47 1.406.75 2.25.75.844 0 1.623-.28 2.25-.75a3.75 3.75 0 0 0 4.902-5.652l-1.3-1.299a1.875 1.875 0 0 0-1.325-.549H5.223Z" />
+                                <path fill-rule="evenodd" d="M3 20.25v-8.755c1.42.674 3.08.673 4.5 0A5.234 5.234 0 0 0 9.75 12c.804 0 1.568-.182 2.25-.506a5.234 5.234 0 0 0 2.25.506c.804 0 1.567-.182 2.25-.506 1.42.674 3.08.675 4.5.001v8.755h.75a.75.75 0 0 1 0 1.5H2.25a.75.75 0 0 1 0-1.5H3Zm3-6a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75v3a.75.75 0 0 1-.75.75h-3a.75.75 0 0 1-.75-.75v-3Zm8.25-.75a.75.75 0 0 0-.75.75v5.25c0 .414.336.75.75.75h3a.75.75 0 0 0 .75-.75v-5.25a.75.75 0 0 0-.75-.75h-3Z" clip-rule="evenodd" />
+                            </svg>
+                            <span className='flex px-3 py-2 text-lg font-medium text-black'>Merch Matrix</span>                    
+                        </div>
+                        <div className="flex flex-1 justify-center">
+                            <div className="flex space-x-4 items-center">
+                                <Link to="/home" className="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-gray-700 hover:text-white" aria-current="page"> Home </Link>
+                                <Link to="/marketplace" className="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-gray-700 hover:text-white"> Store </Link>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                        <button type="button" class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white-800 focus:ring-offset-2 focus:ring-offset-gray-800">
+                            <span class="absolute -inset-1.5"></span>
+                            <span class="sr-only">View notifications</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
+                            </svg>
+                        </button>
+
+                        {/* <!-- Profile dropdown --> */}
+                        <div class="relative ml-3">
+                            <div>
+                                <button 
+                                    type="button" 
+                                    class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" 
+                                    id="user-menu-button" aria-expanded="false" aria-haspopup="true"
+                                    onMouseEnter={() => setOpenProfile(true)}
+                                    onMouseLeave={() => setOpenProfile(false)}
+                                >
+                                    <span class="absolute -inset-1.5"></span>
+                                    <span class="sr-only">Open user menu</span>
+                                    <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
+                                </button>
+                            </div>
+
+                            {openProfile ? (
+                                <div 
+                                    class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" 
+                                    role="menu" 
+                                    aria-orientation="vertical" 
+                                    aria-labelledby="user-menu-button" 
+                                    tabindex="-1"
+                                    onMouseEnter={() => setOpenProfile(true)}
+                                    onMouseLeave={() => setOpenProfile(false)}
+                                >
+                                    <Link to="/#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</Link>
+                                    <Link to="/cart" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">My Cart</Link>
+                                    <Link to="/home" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</Link>
+                                </div>
+                            ) : (
+                                null
+                            )}
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* <!-- Mobile menu, show/hide based on menu state. --> */}
+            
+        </nav>
+
+    )
+}
+
+export default Navbar
+
+
+
